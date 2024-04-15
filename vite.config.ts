2bedbb900e5d4f0a7f3d4fd1741ef662b4b3354d
Julian O'Grady <julianogrady07@gmail.com>
2024-04-15 17:02:36 -0400

    more pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePWA } from 'vite-plugin-pwa';
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +9,54 @@ export default defineConfig({
         includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png, mask-icon.svg'],
         manifest: {
             name: 'ActualCalc',
+            id: 'com.umi-l.ActualCalc',
+            display: 'standalone',
+            lang: "en",
+            orientation: "any",
+            dir: "ltr",
+            scope: "https://umi-l.github.io/ActualCalc/",
+            categories: ['utilities', 'productivity'],
+            screenshots: [
+                {
+                    src: 'screenshot (1).PNG',
+                    sizes: '2266x1488',
+                    type: 'image/png'
+                },
+                {
+                    src: 'screenshot (2).PNG',
+                    sizes: '1488x2266',
+                    type: 'image/png'
+                },
+                {
+                    src: 'screenshot (3).PNG',
+                    sizes: '1488x2266',
+                    type: 'image/png'
+                },
+                {
+                    src: 'screenshot (4).PNG',
+                    sizes: '2266x1488',
+                    type: 'image/png'
+                },
+                {
+                    src: 'screenshot (5).PNG',
+                    sizes: '2266x1488',
+                    type: 'image/png'
+                },
+                {
+                    src: 'screenshot (6).PNG',
+                    sizes: '2266x1488',
+                    type: 'image/png'
+                },
+                {
+                    src: 'screenshot (7).PNG',
+                    sizes: '2266x1488',
+                    type: 'image/png'
+                },
+            ],
             short_name: 'ActualCalc',
             description: 'A calculator that actually works.',
             theme_color: '#191820',
+            background_color: '#191820',
             icons: [
                 {
                     src: 'pwa-64x64.png',
@@ -38,6 +83,5 @@ export default defineConfig({
             ],
         }
     })],
-    base: 'https://umi-l.github.io/ActualCalc/',
-    // base: '/ActualCalc/',
+    base: '/ActualCalc/',
 })
